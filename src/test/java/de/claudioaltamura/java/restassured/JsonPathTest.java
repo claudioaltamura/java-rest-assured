7bae70eb83672fa62b551359ace301e43e8af4d1
Claudio Altamura <claudio@claudioaltamura.de>
2020-08-25 12:06:03 +0200

    chg: upgrade gradle and dep

--- src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
@@ -3,6 +3,7 @@ package de.claudioaltamura.java.restassured;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.when;
 import static io.restassured.path.json.JsonPath.from;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
@@ -13,9 +14,18 @@ class JsonPathTest {
   void findAll() {
     String response = get("http://restcountries.eu/rest/v1/").asString();
 
-    List<String> largeCountries = from(response).getList("findAll { it.population > 100000000 }");
+    List<String> largeCountries = from(response).getList("findAll { it.population > 100000000 }.name");
 
     assertThat(largeCountries).isNotEmpty().hasSize(12);
+  }
+
+  @Test
+  void sumUpNamesLengthWithJsonPath() {
+    String response = get("http://restcountries.eu/rest/v1/").asString();
+
+    int sumNames = from(response).getInt("name*.length().sum()");
+
+    assertThat(sumNames).isEqualTo(12);
   }
 
 }
