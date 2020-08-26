b2b2735fca29198b700b6d935b48fd9a64f637f0
Claudio Altamura <claudio@claudioaltamura.de>
2020-08-26 14:38:02 +0200

    chore: cleaned up

--- src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
@@ -1,12 +1,11 @@
 package de.claudioaltamura.java.restassured;
 
-import java.util.List;
-import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.when;
 import static io.restassured.path.json.JsonPath.from;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class JsonPathTest {
 
@@ -14,7 +13,8 @@ class JsonPathTest {
   void findAll() {
     String response = get("http://restcountries.eu/rest/v1/").asString();
 
-    List<String> largeCountries = from(response).getList("findAll { it.population > 100000000 }.name");
+    List<String> largeCountries =
+        from(response).getList("findAll { it.population > 100000000 }.name");
 
     assertThat(largeCountries).isNotEmpty().hasSize(12);
   }
@@ -27,5 +27,4 @@ class JsonPathTest {
 
     assertThat(sumNames).isEqualTo(2490);
   }
-
 }
