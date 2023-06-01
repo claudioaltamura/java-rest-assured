f26baab4a96fabfd049262ed394e1ffa4b955df5
Claudio Altamura <claudio@claudioaltamura.de>
2023-06-01 20:03:53 +0200

    chore(upgrade): housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,27 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    eclipse
-    idea
-    id("com.diffplug.spotless") version "6.5.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.diffplug.spotless") version "6.19.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
 
-    testImplementation("io.rest-assured:rest-assured:5.0.1")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
 spotless {
