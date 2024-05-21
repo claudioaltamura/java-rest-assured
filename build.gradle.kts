7715d7c8ba5b9da3eaca5772f3ac5bfa7aea3904
Claudio Altamura <claudio@claudioaltamura.de>
2024-05-21 17:54:08 +0200

    build: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     java
-    id("com.diffplug.spotless") version "6.23.0"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.diffplug.spotless") version "6.25.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 repositories {
@@ -12,15 +10,15 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
 dependencies {
-    testImplementation("io.rest-assured:rest-assured:5.3.2")
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
@@ -34,11 +32,7 @@ spotless {
     }
 }
 
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging {
-            events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-        }
-    }
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
 }
