4fed1bde6165fe0086cd16ce048ef50edb160220
Claudio Altamura <claudio@claudioaltamura.de>
2024-05-22 17:49:32 +0200

    refactor: response time test

--- src/test/java/de/claudioaltamura/java/restassured/ResponseTimeTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/ResponseTimeTest.java
@@ -1,9 +1,8 @@
 package de.claudioaltamura.java.restassured;
 
 import static io.restassured.RestAssured.get;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.lessThan;
 
-import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +11,7 @@ class ResponseTimeTest {
   @Disabled("response time is never 1ms")
   @Test
   void shouldFailIfMeasuredResponseTimeIsNotOk() {
-    var response = get("https://swapi.dev/api/people/2/?format=json");
-
-    assertEquals(1, response.timeIn(TimeUnit.MILLISECONDS));
+    var thresholdInMs = 100L;
+    get("https://swapi.dev/api/people/2/?format=json").then().time(lessThan(thresholdInMs));
   }
 }
