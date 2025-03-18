6be66716749e091437fef8bc33ac44176d3893a2
claudioaltamura <claudio@claudioaltamura.de>
2025-03-18 08:59:18 +0100

    fix(test): downgraded libs
    outputdirectory could not be foun

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.2"
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
@@ -18,10 +18,10 @@ dependencies {
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     testImplementation("io.rest-assured:rest-assured:5.5.1")
     testImplementation("org.hamcrest:hamcrest:3.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
-    testImplementation("org.assertj:assertj-core:3.27.3")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.1")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
 
 spotless {
