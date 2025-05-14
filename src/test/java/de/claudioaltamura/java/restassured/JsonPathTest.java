3a6e40d73941284e85bbafd0906fed0fecf6074b
claudioaltamura <claudio@claudioaltamura.de>
2025-05-14 17:53:41 +0200

    fix(JsonPathTest): updated links and logic
    Why has this change been made?
    - SWAPI moved to https://swapi.tech/
    - Spec has slightly changed
    
    See https://swapi.tech/

--- src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
@@ -11,20 +11,21 @@ class JsonPathTest {
 
   @Test
   void shouldFindAllStarshipWithSpecificRating() {
-    String response = get("https://swapi.dev/api/starships?format=json").asString();
+    String response = get("https://swapi.tech/api/starships?format=json").asString();
 
     List<String> starshipWithSpecificRating =
-        from(response).getList("results.findAll{it.hyperdrive_rating == '1.0'}.name");
+        from(response).getList("results.findAll{it.uid >= '2'}.name");
 
-    assertThat(starshipWithSpecificRating).isNotEmpty().hasSize(4);
+    assertThat(starshipWithSpecificRating)
+            .isNotEmpty()
+            .hasSize(4);
   }
 
   @Test
   void shouldResponseAllFilmsAnd200() {
     var films =
-        get("http://swapi.dev/api/films/?format=json").then().statusCode(200).extract().jsonPath();
-    assertThat(films.getList("results")).hasSize(6);
-    assertThat(films.getList("results.title"))
+        get("http://swapi.tech/api/films/?format=json").then().statusCode(200).extract().jsonPath();
+    assertThat(films.getList("result.properties.title"))
         .contains(
             "A New Hope",
             "The Empire Strikes Back",
