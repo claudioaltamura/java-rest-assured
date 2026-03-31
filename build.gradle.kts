ca9c5e88e8c70ebd606218003fa5a6e6a2a6e6c3
claudioaltamura <claudio@claudioaltamura.de>
2026-03-31 09:16:57 +0200

    chore: upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.diffplug.spotless") version "8.0.0"
+    id("com.diffplug.spotless") version "8.4.0"
     id("com.github.ben-manes.versions") version "0.53.0"
 }
 
@@ -24,10 +24,10 @@ dependencies {
     // Ensure JUnit Platform launcher is available on the test runtime classpath
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
-    testImplementation("io.rest-assured:rest-assured:5.5.6")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.21.2")
+    testImplementation("io.rest-assured:rest-assured:6.0.0")
     testImplementation("org.hamcrest:hamcrest:3.0")
-    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 spotless {
