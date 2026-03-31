d7722b19f9c027ff79fe0dd0f397c71168a5f89a
claudioaltamura <claudio@claudioaltamura.de>
2026-03-31 09:17:11 +0200

    chg: moved test

--- src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/JsonPathTest.java
@@ -18,38 +18,4 @@ class JsonPathTest {
 
     assertThat(starshipWithSpecificRating).isNotEmpty().hasSize(4);
   }
-
-  /*
-  @Test
-  void shouldResponseAllFilmsAnd200() {
-    var films =
-        given()
-            .when()
-            .get("http://swapi.tech/api/films/?format=json")
-            .then()
-            .assertThat()
-            .statusCode(200)
-            .and()
-            .extract()
-            .jsonPath();
-
-    assertThat(films.getList("result.properties.title"))
-        .contains(
-            "A New Hope",
-            "The Empire Strikes Back",
-            "Return of the Jedi",
-            "The Phantom Menace",
-            "Attack of the Clones",
-            "Revenge of the Sith");
-  }
-   */
-
-  /*
-   * @Test void sumUpNamesLengthWithJsonPath() { String response =
-   * get("http://restcountries.eu/rest/v1/").asString();
-   *
-   * <p>int sumNames = from(response).getInt("name*.length().sum()");
-   *
-   * <p>assertThat(sumNames).isEqualTo(2490); }
-   */
 }
--- src/test/java/de/claudioaltamura/java/restassured/SimpleTest.java
+++ src/test/java/de/claudioaltamura/java/restassured/SimpleTest.java
@@ -1,11 +1,35 @@
 package de.claudioaltamura.java.restassured;
 
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 import org.junit.jupiter.api.Test;
 
 class SimpleTest {
+
+  @Test
+  void shouldResponseAllFilmsAnd200() {
+    var films =
+        given()
+            .when()
+            .get("http://swapi.tech/api/films/?format=json")
+            .then()
+            .assertThat()
+            .statusCode(200)
+            .and()
+            .extract()
+            .jsonPath();
+
+    assertThat(films.getList("result.properties.title"))
+        .contains(
+            "A New Hope",
+            "The Empire Strikes Back",
+            "Return of the Jedi",
+            "The Phantom Menace",
+            "Attack of the Clones",
+            "Revenge of the Sith");
+  }
 
   @Test
   void shouldResponse200AndJsonHasRequiredKV() {
